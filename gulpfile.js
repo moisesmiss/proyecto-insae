0c3739a9ef7e59119b24a407351c4c212be29a3a
moisesmiss <moisesgamiss@gmail.com>
2018-09-24 22:47:20 -0500

    creación del login con un diseño basico en bootstrap

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+var wait = require('gulp-wait');
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./" //RUTA PRINCIPAL DONDE SE INICIALIZARÁ EL SERVIDOR
+    });
+
+    gulp.watch("./scss/*.scss", ['sass']); //RUTA DE LA CARPETA DE SASS ORIGEN
+    gulp.watch("./*.html").on('change', browserSync.reload); //WATCH DE LOS ARCHIVOS HTML
+    gulp.watch("./*.php").on('change', browserSync.reload); //WATCH DE LOS ARCHIVOS PHP
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("./scss/*.scss") //CARPETA SCSS ORIGEN
+        .pipe(wait(100))
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest("./css/")) //CARPETA CSS DESTINO
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
