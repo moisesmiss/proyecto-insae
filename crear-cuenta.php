149ba25da7695dfac375b47c54d5c3eeccf6fc52
moisesmiss <moisesgamiss@gmail.com>
2018-11-11 20:31:15 -0600

    cambio de diseño

--- crear-cuenta.php
+++ crear-cuenta.php
@@ -2,100 +2,59 @@
 <html lang="es">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<link rel="stylesheet" type="text/css" href="css/all.min.css">
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="css/mdb.min.css">
-	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<title>Sistema de ventas e inventario</title>
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap-grid.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap-utilities.css">
+	<link rel="stylesheet" type="text/css" href="materialize/css/materialize.css">
 </head>
 <body>
 	<div class="container">
-		<div class="login row d-flex justify-content-center">
-			<div class="col-12 col-md-8">
-				<!-- Material form register -->
-				<div class="card">
-
-					<h5 class="card-header info-color white-text text-center py-4">
-						<strong>Registrarse</strong>
-					</h5>
-
-					<!--Card content-->
-					<div class="card-body px-lg-5 pt-0">
-
-						<!-- Form -->
-						<form class="text-center" style="color: #757575;">
-
-							<div class="form-row">
-								<div class="col-12 col-md">
-									<!-- nombre -->
-									<div class="md-form">
-										<input type="text" id="materialRegisterFormFirstName" class="form-control">
-										<label for="materialRegisterFormFirstName">Nombre</label>
-									</div>
-								</div>
-								<div class="col-12 col-md">
-									<!-- apellido paterno -->
-									<div class="md-form">
-										<input type="email" id="materialRegisterFormLastName" class="form-control">
-										<label for="materialRegisterFormLastName">Apellido Paterno</label>
-									</div>
-								</div>
-								<div class="col-12 col-md">
-									<!-- apellido materno -->
-									<div class="md-form">
-										<input type="text" name="" id="apellidoM" class="form-control">
-										<label for="apellidoM">Apellido Materno</label>
-									</div>
-								</div>
-							</div>
-
-							<!-- E-mail -->
-							<div class="md-form mt-0">
-								<input type="email" id="materialRegisterFormEmail" class="form-control">
-								<label for="materialRegisterFormEmail">Correo</label>
-							</div>
-
-							<div class="form-row">
-								<!-- Password -->
-								<div class="col-12 col-md">
-									<div class="md-form">
-										<input type="password" id="password" class="form-control" aria-describedby="materialRegisterFormPasswordHelpBlock">
-										<label for="password">Contraseña</label>
-									</div>
-								</div>
-
-								<div class="col-12 col-md">
-									<div class="md-form">
-										<input type="password" id="password2" class="form-control" aria-describedby="materialRegisterFormPasswordHelpBlock">
-										<label for="password2">Repetir Contraseña</label>
-									</div>
-								</div>
-								
-							</div>
-
-
-							<!-- Sign up button -->
-							<button class="btn btn-outline-info btn-rounded btn-block my-4 waves-effect z-depth-0" type="submit">Aceptar</button>
-							<a href="index.php">Iniciar sesión</a>
-
-							</form>
-							<!-- Form -->
-
+		<div class="row justify-content-center">
+			<div class="col-12 col-md-4">
+				<h5 class="text-center my-4">Crear cuenta</h5>
+				<form class="row">
+					<div class="col-12">
+						<div class="input-field">
+							<input type="text" name="nombre" id="nombre">
+							<label for="nombre">Nombre</label>
 						</div>
-
 					</div>
-					<!-- Material form register -->
-				</div>
+					<div class="col-12">
+						<div class="input-field">
+							<input type="text" name="apellidos" id="apellidos">
+							<label for="apellidos">Apellidos</label>
+						</div>
+					</div>
+					<div class="col-12">
+						<div class="input-field">
+							<input type="email" name="email" id="email">
+							<label for="email">Correo</label>
+						</div>
+					</div>
+					<div class="col-12">
+						<div class="input-field">
+							<input type="password" name="password" id="password">
+							<label for="password">Contraseña</label>
+						</div>
+					</div>
+					<div class="col-12">
+						<div class="input-field">
+							<input type="password" name="password2" id="password2">
+							<label for="password2">Repetir Contraseña</label>
+						</div>
+					</div>
+					<div class="col-12">
+						<button class="btn mx-auto d-block" type="submit">Aceptar</button>
+					</div>
+					<div class="col-12 my-4">
+						<a class="d-block text-center" href="index.php">Iniciar sesión</a>
+					</div>
+				</form>
 			</div>
-
-
-
-
 		</div>
-		<script type="text/javascript" src="js/popper.min.js"></script>
-		<script type="text/javascript" src="js/jquery.min.js"></script>
-		<script type="text/javascript" src="js/bootstrap.min.js"></script>
-		<script type="text/javascript" src="js/mdb.min.js"></script>
-	</body>
-	</html>
+	</div>
+
+	<script type="text/javascript" src="materialize/js/materialize.min.js"></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -2,81 +2,33 @@
 <html lang="es">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<link rel="stylesheet" type="text/css" href="css/all.min.css">
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="css/mdb.min.css">
-	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<title>Sistema de ventas e inventario</title>
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap-grid.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap-utilities.css">
+	<link rel="stylesheet" type="text/css" href="materialize/css/materialize.css">
 </head>
 <body>
 	<div class="container">
-		<div class="login row d-flex justify-content-center">
-			<div class="col-12 col-md-6">
-				<!-- Material form login -->
-				<div class="card">
-
-					<h5 class="card-header info-color white-text text-center py-4">
-						<strong>Iniciar sesión</strong>
-					</h5>
-
-					<!--Card content-->
-					<div class="card-body px-lg-5 pt-0">
-
-						<!-- Form -->
-						<form class="text-center" style="color: #757575;">
-
-							<!-- Email -->
-							<div class="md-form">
-								<input type="email" id="materialLoginFormEmail" class="form-control">
-								<label for="materialLoginFormEmail">Correo</label>
-							</div>
-
-							<!-- Password -->
-							<div class="md-form">
-								<input type="password" id="materialLoginFormPassword" class="form-control">
-								<label for="materialLoginFormPassword">Contraseña</label>
-							</div>
-
-							<div class="d-flex justify-content-around">
-								<div>
-									<!-- Remember me -->
-									<div class="form-check">
-										<input type="checkbox" class="form-check-input" id="materialLoginFormRemember">
-										<label class="form-check-label" for="materialLoginFormRemember">Recuerdame</label>
-									</div>
-								</div>
-								<div>
-									<!-- Forgot password -->
-									<a href="">¿Olvidaste tu contraseña?</a>
-								</div>
-							</div>
-
-							<!-- Sign in button -->
-							<button class="btn btn-outline-info btn-rounded btn-block my-4 waves-effect z-depth-0" type="submit">Iniciar Sesión</button>
-
-							<!-- Register -->
-							<p>
-								<a href="crear-cuenta.php">Crear cuenta</a>
-							</p>
-
-						</form>
-						<!-- Form -->
-
+		<div class="row justify-content-center">
+			<div class="col-12 col-md-4">
+				<h5 class="text-center my-4">Iniciar sesión</h5>
+				<form>
+					<div class="input-field">
+						<input type="email" name="email" id="email">
+						<label for="email">Correo</label>
 					</div>
-
-				</div>
-				<!-- Material form login -->
+					<div class="input-field">
+						<input type="password" name="password" id="password">
+						<label for="password">Contraseña</label>
+					</div>
+					<button class="btn mx-auto d-block" type="submit">Aceptar</button>
+					<a class="mt-4 d-block text-center" href="crear-cuenta.php">Crear cuenta</a>
+				</form>
 			</div>
 		</div>
-
-
-
-		
 	</div>
-	<script type="text/javascript" src="js/popper.min.js"></script>
-	<script type="text/javascript" src="js/jquery.min.js"></script>
-	<script type="text/javascript" src="js/bootstrap.min.js"></script>
-	<script type="text/javascript" src="js/mdb.min.js"></script>
+
+	<script type="text/javascript" src="materialize/js/materialize.min.js"></script>
 </body>
 </html>
